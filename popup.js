d526b367c1b00cb3ae7771b1a6ecfa61df896129
NHirt32 <nicholas.t.hirt@gmail.com>
2025-10-05 11:29:55 -0700

    Made changes for translate to popup(html+js) and message passing on content.js

--- popup.js
+++ popup.js
@@ -11,16 +11,38 @@ function setStoredSelectedMode(mode) {
     chrome.storage.local.set({ selectedMode: mode });
 }
 
+// Functions for getting saved translation (No awaits in popup again...)
+function getStoredLanguage() {
+    return new Promise((resolve) => {
+        chrome.storage.local.get("selectedLanguage", (result) => {
+            resolve(result.selectedLanguage || "en"); // default English
+        });
+    });
+}
+
+function setStoredLanguage(lang) {
+    chrome.storage.local.set({ selectedLanguage: lang });
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
     const readButton = document.getElementById("read");
     const stopButton = document.getElementById("stop");
     const pauseButton = document.getElementById("pause");
     const resumeButton = document.getElementById("resume");
     const modeSelect = document.getElementById("mode");
+    const langSelect = document.getElementById("targetLang");
 
     // Load saved mode from storage, had to change document event listener to async
     const storedMode = await getStoredSelectedMode();
     if (modeSelect) modeSelect.value = storedMode;
+    const storedLang = await getStoredLanguage();
+    if (langSelect) langSelect.value = storedLang;
+
+    // Listener for detecting change in language dropdown
+    langSelect.addEventListener("change", () => {
+        const selectedLang = langSelect.value;
+        setStoredLanguage(selectedLang);
+    });
 
     // Listener for detecting change in mode dropdown
     modeSelect.addEventListener("change", () => {
@@ -30,13 +52,16 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     readButton?.addEventListener("click", () => {
         const selectedMode = modeSelect?.value || "Oliver"; // "Oliver", "Paige", or "Annie"
+        const selectedLang = langSelect?.value || "en"; // e.g. en, hi, pt
         console.log("Popup sending mode:", selectedMode);
 
         sendToActiveTab({
             type: "GET_SELECTION_AND_NARRATE",
-            mode: selectedMode
+            mode: selectedMode,
+            lang: selectedLang
         }); 
     });
+
 
     stopButton?.addEventListener("click", () => {
         sendToActiveTab({ type: "STOP_AUDIO" });
